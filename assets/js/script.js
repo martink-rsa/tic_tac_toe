d76834e790820dda04619d7b597a0d023055b9e7
Martin Kruger <martin@martinkruger.com>
2019-09-08 18:33:59 +0200

    Basic win conditions check

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,163 @@
 /* eslint-disable no-console */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-unused-vars */
+
 console.clear();
+'use strict';
+
 // https://www.theodinproject.com/courses/javascript/lessons/tic-tac-toe-javascript?ref=lnav
 
-// 1. Create HCJ backbone
+const Player = (name, mark) => {
+  let _name = name;
+  let _mark = mark;
+  const getName = () => _name;
+  const setName = (newName) => { _name = newName; };
 
-// 2: Store gameboard as array inside of Gameboard object.
-// 2.1 Players going to be stored in objects
-// 2.2 Object to control the flow of the game
-// 2 Summary: No global code or little as possible.
-//      One instance:       Use module
-//      Multiple instances: Use factories
+  const getMark = () => _mark;
+  const setMark = (newMark) => { _mark = newMark; };
 
-// 3. JS Function: Render contents of gameboard array
-//      to the webpage.
+  const displayDetails = () => console.log(`${getName()} ${getMark()}`);
 
-
-const gridElements = document.getElementsByClassName('mark-container');
-
-const GameBoard = (() => {
-
-  const generateGameBoard = () => 'Board generated';
-
-  const updateGameBoard = (grid) => {
-    for (let i = 0; i < gridElements.length; i += 1) {
-      gridElements[i].textContent = grid[i];
-    }
-  };
-
-  return {
-    generateGameBoard,
-    updateGameBoard,
-  };
-})();
-
-const GameMain = (() => {
-  let markGrid = ['', '', '', '', '', '', '_', '_', '_'];
-
-  const getGrid = () => markGrid;
-
-  const playMark = (positionPlayed) => {
-    markGrid[positionPlayed] = Player.getMark();
-    GameBoard.updateGameBoard(GameMain.getGrid());
-  };
-
-  const turnPlayed = (event) => {
-    playMark(event.target.getAttribute('data-value'));
-  };
-
-  return {
-    turnPlayed,
-    getGrid,
-  }
-})();
-
-for (let i = 0; i < gridElements.length; i += 1) {
-  gridElements[i].addEventListener('click', GameMain.turnPlayed);
-}
-
-const Player = (() => {
-  let name = 'Player 1';
-  let mark = '0';
-
-  const getName = () => name;
-  const setName = (newName) => { name = newName; };
-
-  const getMark = () => mark;
-  const setMark = (newMark) => { mark = newMark; };
-
-  return {
+  return Object.freeze({
     getName,
     setName,
     getMark,
     setMark,
-  }
+    displayDetails,
+  });
+};
+
+const GameBoard = (() => {
+  const _gridElements = document.getElementsByClassName('mark-container');
+
+  const updateGameBoard = (gridMarks) => {
+    const marks = gridMarks.flat();
+    for (let i = 0; i < marks.length; i += 1) {
+      _gridElements[i].textContent = marks[i];
+    }
+  };
+
+  const gameElementClicked = (e) => {
+    const positionPlayed = e.target.getAttribute('data-value');
+    GameMain.playTurn(positionPlayed);
+  };
+
+  const initGrid = () => {
+    for (let i = 0; i < _gridElements.length; i += 1) {
+      _gridElements[i].addEventListener('click', gameElementClicked);
+    }
+  };
+
+  return {
+    update: updateGameBoard,
+    init: initGrid,
+  };
 })();
 
-console.log(Player.getName());
+const GameMain = (() => {
+  let _grid = [['', '', ''], ['', '', ''], ['_', '_', '_']];
+  let _players = [];
+  let _currentTurn = 0;
 
-GameBoard.updateGameBoard(GameMain.getGrid());
+  const getGrid = () => _grid;
+  const setGrid = (grid) => {
+    _grid = grid.slice(0);
+  };
+
+  const getPlayers = () => _players;
+  const setPlayers = (players) => {
+    _players = players.slice(0);
+  };
+
+  const getCurrentTurn = () => _currentTurn;
+  const newTurn = () => {
+    _currentTurn += 1;
+  };
+  const resetTurns = () => {
+    _currentTurn = 0;
+  };
+
+  const newGame = (...players) => {
+    const newPlayers = [];
+    for (let i = 0; i < players.length; i += 1) {
+      newPlayers.push(players[i]);
+    }
+    setPlayers(newPlayers);
+  };
+
+  const drawMark = (positionPlayed, currentPlayer) => {
+    const row = parseInt(positionPlayed / 3, 10);
+    const column = positionPlayed % 3;
+    _grid[row][column] = currentPlayer.getMark();
+    GameBoard.update(GameMain.getGrid());
+  };
+
+  const checkWinConditions = (player) => {
+    const grid = getGrid();
+    const mark = player.getMark();
+    // COLUMNS
+    for (let i = 0; i < grid.length; i += 1) {
+      if (grid[0][i] === mark && grid[1][i] === mark && grid[2][i] === mark) {
+        return true;
+      }
+    }
+    // ROWS
+    for (let i = 0; i < grid.length; i += 1) {
+      if (grid[i][0] === mark && grid[i][1] === mark && grid[i][2] === mark) {
+        return true;
+      }
+    }
+    // DIAGONAL - RIGHT
+    if (grid[0][0] === mark && grid[1][1] === mark && grid[2][2] === mark) {
+      return true;
+    }
+    // DIAGONAL - LEFT
+    if (grid[0][2] === mark && grid[1][1] === mark && grid[2][0] === mark) {
+      return true;
+    }
+    return false;
+  };
+
+  const playTurn = (positionPlayed) => {
+    if (getCurrentTurn() === getGrid().flat().length) {
+      console.log('GAME END');
+    } else {
+      console.log('Current round is: ' + (getCurrentTurn() + 1))
+      const currentPlayerIndex = getCurrentTurn() % getPlayers().length;
+      const players = getPlayers();
+      drawMark(positionPlayed, players[currentPlayerIndex]);
+      if (checkWinConditions(players[currentPlayerIndex])) {
+        alert(players[currentPlayerIndex].getName() + ' wins!');
+      }
+      newTurn();
+    }
+  };
+
+  const resetGame = () => {
+    setGrid([['', '', ''], ['', '', ''], ['', '', '']]);
+    GameBoard.update(getGrid());
+    resetTurns();
+  };
+
+  return {
+    newGame,
+    getGrid,
+    drawMark,
+    playTurn,
+    resetGame,
+  };
+})();
+
+
+GameBoard.init();
+GameBoard.update(GameMain.getGrid());
+
+const playerOne = Player('Player 1', 'O');
+const playerTwo = Player('Player 2', 'X');
+
+GameMain.newGame(playerOne, playerTwo);
+
+document.getElementById('btn-reset-game').addEventListener('click', GameMain.resetGame);
 
 
 /* TEST UNITS */
@@ -88,4 +168,7 @@ function testOutput(input) {
 
 module.exports = {
   testOutput,
+  Player,
+  GameBoard,
+  GameMain,
 };
