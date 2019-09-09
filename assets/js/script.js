fddcfc6263fa15ce9203513dfd88f54de8a95963
Martin Kruger <martin@martinkruger.com>
2019-09-09 11:57:43 +0200

    Animated and glowing icons. Had to add SVGs through innerHTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,13 +27,74 @@ const Player = (name, mark) => {
   });
 };
 
+const playerOne = Player('Player 1', 'o');
+const playerTwo = Player('Player 2', 'x');
+
 const GameBoard = (() => {
   const _gridElements = document.getElementsByClassName('mark-container');
 
-  const updateGameBoard = (gridMarks) => {
+  const toggleTest = () => {
+    console.log('TOGGLE');
+    console.log(_gridElements[0].children[0].classList);
+    console.log(_gridElements[0].getElementsByTagName('circle'));
+    const noughtCircles = _gridElements[0].getElementsByTagName('circle');
+    noughtCircles[0].classList.toggle('nought-show');
+    noughtCircles[1].classList.toggle('nought-show');
+
+  }
+
+  const updateGameBoard = (gridMarks, positionPlayed) => {
+    console.log(positionPlayed);
     const marks = gridMarks.flat();
+    let noughtClass;
+    let crossClass;
+    let currentContainer;
+    let currentPositionClass = '';
+    if (playerOne.getMark().toLowerCase() === 'o') {
+      noughtClass = 'player-one';
+      crossClass = 'player-two';
+    } else {
+      noughtClass = 'player-two';
+      crossClass = 'player-one';
+    }
     for (let i = 0; i < marks.length; i += 1) {
-      _gridElements[i].textContent = marks[i];
+      if (currentPositionClass !== -1 && Number(positionPlayed) === i) {
+        currentPositionClass = '';
+      } else {
+        if (marks[i].toLowerCase() === 'o') {
+          currentPositionClass = 'nought-show';
+        } else if (marks[i].toLowerCase() === 'x') {
+          currentPositionClass = 'cross-show';
+        } else {
+          currentPositionClass = '';
+        }
+      }
+      console.log(i + ": " + currentPositionClass);
+      currentContainer = _gridElements[i];
+      if (marks[i].toLowerCase() === 'o') {
+        currentContainer.innerHTML = '<svg class="' + noughtClass + '" height="100" width="100" viewBox="0 0 100 100"><g><defs><filter id="glow" x="-30%" y="-30%" width="160%" height="160%"><feGaussianBlur stdDeviation="4 4" result="glow"/><feMerge><feMergeNode in="glow"/><feMergeNode in="glow"/><feMergeNode in="glow"/></feMerge></filter></defs><circle style="filter: url(#glow); opacity: 0.75;" class="nought ' + currentPositionClass + '" cx="50" cy="50" r="30"/><circle class="nought ' + currentPositionClass + '" cx="50" cy="50" r="30"/></g></svg>';   
+      } else if (marks[i].toLowerCase() === 'x') {
+        currentContainer.innerHTML = '<svg class="' + crossClass + '" height="100" width="100" viewBox="0 0 100 100"><g><defs><filter id="glow" x="-30%" y="-30%" width="160%" height="160%"><feGaussianBlur stdDeviation="4 4" result="glow"/><feMerge><feMergeNode in="glow"/><feMergeNode in="glow"/><feMergeNode in="glow"/></feMerge></filter></defs><line style="filter: url(#glow); opacity: 0.75;" class="cross ' + currentPositionClass + '" x1="20" y1="20" x2="80" y2="80"/><line style="filter: url(#glow);" class="cross cross-delay ' + currentPositionClass + '" x1="80" y1="20" x2="20" y2="80"/><line class="cross ' + currentPositionClass + '" x1="20" y1="20" x2="80" y2="80"  /><line class="cross cross-delay ' + currentPositionClass + '" x1="80" y1="20" x2="20" y2="80" /></g></svg>';
+      } else {
+        currentContainer.innerHTML = '';
+      }
+    }
+    if (positionPlayed !== -1) {
+      if (marks[positionPlayed].toLowerCase() === 'o') {
+        const noughtCircles = _gridElements[positionPlayed].getElementsByTagName('circle');
+        setTimeout(() => {
+          noughtCircles[0].classList.toggle('nought-show');
+          noughtCircles[1].classList.toggle('nought-show');
+        }, 100);
+      } else if (marks[positionPlayed].toLowerCase() === 'x') {
+        const crossLines = _gridElements[positionPlayed].getElementsByTagName('line');
+        setTimeout(() => {
+          crossLines[0].classList.toggle('cross-show');
+          crossLines[1].classList.toggle('cross-show');
+          crossLines[2].classList.toggle('cross-show');
+          crossLines[3].classList.toggle('cross-show');
+        }, 100);
+      }
     }
   };
 
@@ -51,11 +112,12 @@ const GameBoard = (() => {
   return {
     update: updateGameBoard,
     init: initGrid,
+    toggleTest,
   };
 })();
 
 const GameMain = (() => {
-  let _grid = [['', '', ''], ['', '', ''], ['_', '_', '_']];
+  let _grid = [['', '', ''], ['', '', ''], ['', '', '']];
   let _players = [];
   let _currentTurn = 0;
 
@@ -88,8 +150,11 @@ const GameMain = (() => {
   const drawMark = (positionPlayed, currentPlayer) => {
     const row = parseInt(positionPlayed / 3, 10);
     const column = positionPlayed % 3;
+    console.log({row},{column});
     _grid[row][column] = currentPlayer.getMark();
-    GameBoard.update(GameMain.getGrid());
+    GameBoard.update(GameMain.getGrid(), positionPlayed);
+
+    // CURRENT CHANGES: USE POSITION PLAYED TO ADD ANIMATION TO LAST PIECE PLAYED.
   };
 
   const checkWinConditions = (player) => {
@@ -135,7 +200,7 @@ const GameMain = (() => {
 
   const resetGame = () => {
     setGrid([['', '', ''], ['', '', ''], ['', '', '']]);
-    GameBoard.update(getGrid());
+    GameBoard.update(getGrid(), -1);
     resetTurns();
   };
 
@@ -150,14 +215,15 @@ const GameMain = (() => {
 
 
 GameBoard.init();
-GameBoard.update(GameMain.getGrid());
+GameBoard.update(GameMain.getGrid(), -1);
 
-const playerOne = Player('Player 1', 'O');
-const playerTwo = Player('Player 2', 'X');
+
 
 GameMain.newGame(playerOne, playerTwo);
 
+document.getElementById('btn-play-game').addEventListener('click', GameBoard.toggleTest);
 document.getElementById('btn-reset-game').addEventListener('click', GameMain.resetGame);
+
 
 
 /* TEST UNITS */
